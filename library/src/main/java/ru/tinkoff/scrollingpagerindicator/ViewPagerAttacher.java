e1baf34c5a457bf48e82a76a24c260d6ac27b578
Nikita Olifer <n.olifer@tinkoff.ru>
2018-02-21 13:34:38 +0300

    Check for illegal offset values in ViewPagerAdapter.

--- library/src/main/java/ru/tinkoff/scrollingpagerindicator/ViewPagerAttacher.java
+++ library/src/main/java/ru/tinkoff/scrollingpagerindicator/ViewPagerAttacher.java
@@ -45,7 +45,16 @@ public class ViewPagerAttacher implements ScrollingPagerIndicator.PagerAttacher<
 
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixel) {
-                indicator.onPageScrolled(position, positionOffset);
+                final float offset;
+                // ViewPager may emit negative positionOffset for very fast scrolling
+                if (positionOffset < 0) {
+                    offset = 0;
+                } else if (positionOffset > 1) {
+                    offset = 1;
+                } else {
+                    offset = positionOffset;
+                }
+                indicator.onPageScrolled(position, offset);
             }
 
             @Override
